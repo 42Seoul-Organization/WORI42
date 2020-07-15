088c03d11fb0f37c8392323847d8faf24a28f7d2
hochan222 <hochan049@gmail.com>
2020-07-15 13:01:54 +0900

    Updates

---
+++ precache-manifest.c03ad3b941506ab079cba08be4502170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb31f8a1f59901b9d90532074c79637",
+    "url": "/WORI42/index.html"
+  },
+  {
+    "revision": "2d07c4497451da7e10c5",
+    "url": "/WORI42/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b61dc606442b5fba653",
+    "url": "/WORI42/static/js/2.04a95178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WORI42/static/js/2.04a95178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d07c4497451da7e10c5",
+    "url": "/WORI42/static/js/main.db747e40.chunk.js"
+  },
+  {
+    "revision": "a823d2cccb8bddacaca0",
+    "url": "/WORI42/static/js/runtime-main.b836205d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WORI42/static/media/logo.5d5d9eef.svg"
+  }
+]);
