a225b8b83dd37ef586db3b32ec57a6d53d3ca09e
hochan222 <hochan049@gmail.com>
2020-07-21 08:27:56 +0900

    [ADD] main site link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'react/build')));
