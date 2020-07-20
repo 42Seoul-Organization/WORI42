9d52c364608c2c4cc2b3bd064e32ae46992c9a51
hochan222 <hochan049@gmail.com>
2020-07-21 08:23:42 +0900

    [FIX] PORT

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = env.process.PORT || 3002
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'react/build')));
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
