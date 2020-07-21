011109e614073b824ce3bcf8fd9f9ed6bb4f322b
yujo <yujo@student.42seoul.kr>
2020-07-21 15:14:56 +0900

    [CREATE]Example/Marker_CSS Spread pretty Marker Node on the Map

---
+++ react/src/Component/Example/Marker_CSS/RealNode.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./RealNode.css";
+
+const Dummy = {
+  locate: "Boosan",
+  latitude: 35.17944,
+  longitude: 129.07556,
+};
+
+class RealNode extends React.Component {
+  render() {
+    return <div className="realnode"></div>;
+  }
+}
+
+export default RealNode;
